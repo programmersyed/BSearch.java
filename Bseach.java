42395582b8d4555acba5d9fb3f8e2aff5e2abe36
programmersyed <abrahamsyed10@gmail.com>
2022-06-22 11:03:10 -0400

    Create Bseach.java

---
+++ Bseach.java
@@ -0,0 +1,18 @@
+public class BinarySearch {
+  public static int search(int[] arr, int target) {
+int left = 0;
+int right = arr.length;
+  
+    // Add mid calculation below
+int mid = Math.floorDiv(left + right, 2);
+    return mid;
+  }
+
+  public static void main(String[] args) {
+    int[] searchable = {1, 2, 7, 8, 22, 28, 41, 58, 67, 71, 94};
+    int target = 41;
+
+    System.out.println(search(searchable, target));
+  }
+
+}
